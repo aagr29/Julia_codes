c32a101a034779f5897c806f4b0e90837fbba102
Aniket Agrawal <aniketagrawal.s@gmail.com>
2023-03-01 12:00:58 +1030

    add prob

--- convertjld2toCSV.jl
+++ convertjld2toCSV.jl
@@ -2,6 +2,6 @@ using JLD2#follow install instructions for these libraries
 using StaticArrays
 using TypedTables
 using CSV
-in=load("TasNetworks_filtered_PC_TAS_Networks_Campaign_1-527000_5207500.jld2")
-out=merge(columns(in["pcNetwork"]),(probsNetwork=in["probsNetwork"],))
-CSV.write("temp.csv",in["pcNetwork"])
+input=load("TasNetworks_filtered_PC_TAS_Networks_Campaign_1-527000_5207500.jld2")
+output=merge(columns(input["pcNetwork"]),(probsNetwork=input["probsNetwork"],))
+CSV.write("temp1.csv",output)
